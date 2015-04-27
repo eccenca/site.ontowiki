e2ae6f8b29cfa2725104a94ae5c2f2ad5ae0d2d1
Michael Haschke <mhaschke@brox.de>
2015-04-27 17:45:36 +0200

    [mod] export sitemap xml to dump folder if all necessary parameters are configured (sitemapUri, urlBase and targetPath)

--- jobs/ExportSite.php
+++ jobs/ExportSite.php
@@ -21,6 +21,36 @@ class Site_Job_ExportSite extends Erfurt_Worker_Job_Abstract
         OntoWiki::getInstance()->selectedModel = $model;
 
         $helper->setUrlBase($workload->urlBase);
+        
+        // dump the sitemap.xml
+        
+        if (
+            isset($workload->sitemapUri) &&
+            $workload->sitemapUri && 
+            $sitemapXml = $helper->createSitemapXml()
+           )
+        {
+            $sm_name = substr($workload->sitemapUri, strlen($workload->urlBase));
+            $sm_nameparts = explode('/', $sm_name);
+            $sm_filename = array_pop($sm_nameparts);
+            $sm_dirname = $workload->targetPath . implode('/', $sm_nameparts);
+            
+            if (!is_dir($sm_dirname)) {
+                mkdir($sm_dirname, 0755, TRUE);
+            }
+            
+            if (file_put_contents($sm_dirname . '/' . $sm_filename, $sitemapXml )) {
+                echo sprintf('%s', 'Write ' . $sm_dirname . '/' . $sm_filename) . PHP_EOL;
+                $this->logSuccess(sprintf('%s', 'Write ' . $sm_dirname . '/' . $sm_filename));
+            }
+            else {
+                echo sprintf('%s', 'Cannot write ' . $sm_dirname . '/' . $sm_filename) . PHP_EOL;
+                $this->logError(sprintf('%s', 'Cannot write ' . $sm_dirname . '/' . $sm_filename));
+            }
+            
+        }
+
+        // queue jobs to dump all resources with types that have templates configured
 
         $uris   = $helper->getAllURIs();
         $count  = count($uris);
